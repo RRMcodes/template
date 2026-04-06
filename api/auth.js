324e8d4b3d195bedbd676d3dc984dfd5b97db914
Regan <reganmaharjan.11@gmail.com>
2026-04-07 00:14:18 +0545

    feat: add GitHub OAuth authentication endpoint to api/auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,10 @@
 module.exports = (req, res) => {
   const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+  
+  if (!GITHUB_CLIENT_ID) {
+    return res.status(500).send("Configuration Error: GITHUB_CLIENT_ID is not defined in Vercel Environment Variables.");
+  }
+
   const scope = "repo,user";
   const url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=${scope}`;
   
